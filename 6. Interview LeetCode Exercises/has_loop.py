976f50e072eeaa6277681974f599169a3a4b0917
Jose Junior <h1ddenntc@gmail.com>
2024-09-11 02:43:05 +0100

    implemented has_loop.py

--- 6. Interview LeetCode Exercises/has_loop.py
+++ 6. Interview LeetCode Exercises/has_loop.py
@@ -33,14 +33,13 @@ class LinkedList:
                 return False 
            
     
-    
-    
+ 
 my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
 my_linked_list_1.tail.next = my_linked_list_1.head
-print('Test 1: ', my_linked_list_1.has_loop() ) # Returns True
+print(my_linked_list_1.has_loop() ) # Returns True
 
 
 
@@ -49,7 +48,7 @@ my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
-print('Test 2: ', my_linked_list_2.has_loop() ) # Returns False
+print(my_linked_list_2.has_loop() ) # Returns False
 
 
 
